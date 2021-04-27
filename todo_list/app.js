22c93b84d20153623e405f6641238fd6122e57b3
laichangwen <laichangwen21@gmail.com>
2021-04-28 02:55:21 +0800

    feat: add update todo function

--- todo_list/app.js
+++ todo_list/app.js
@@ -30,16 +30,9 @@ app.get("/", (req, res) => {
     .catch(error => console.error(error))
 })
 
+// create new todo
 app.get("/todos/new", (req, res) => {
-  res.render("new")
-})
-
-app.get("/todos/:id", (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then(todos => res.render("detail", { todos }))
-    .catch(error => console.log(error))
+  return res.render("new")
 })
 
 app.post("/todos", (req, res) => {
@@ -48,6 +41,39 @@ app.post("/todos", (req, res) => {
     .then(() => res.redirect("/"))
     .catch(error => console.log(error))
 })
+
+// show details of a specific id
+app.get("/todos/:id", (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then(todo => res.render("detail", { todo }))
+    .catch(error => console.log(error))
+})
+
+// edit todo of a specific id
+app.get("/todos/:id/edit", (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then(todo => res.render("edit", { todo }))
+    .catch(error => console.log(error))
+})
+
+app.post("/todos/:id/edit", (req, res) => {
+  const name = req.body.name
+  const id = req.params.id
+  return Todo.findById(id)
+    .then(todo => {
+      todo.name = name
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
+})
+
+
+
 
 app.listen(3000, () => {
   console.log("Listening on localhost:3000")
