646b694dc6e26584c1468bc14b3bd26f23e33d4d
laichangwen <laichangwen21@gmail.com>
2021-04-28 00:00:25 +0800

    feat: add mongoose

--- todo_list/app.js
+++ todo_list/app.js
@@ -1,5 +1,19 @@
 const express = require("express")
+const mongoose = require("mongoose")
 const app = express()
+
+mongoose.connect("mongodb://localhost/todo-list", { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+// fail to connect
+db.on("error", () => {
+  console.log("mongodb error!!!")
+})
+// succeed in connecting
+db.once("open", () => {
+  console.log("mongodb connected!!!")
+})
 
 app.get("/", (req, res) => {
   res.send("Hello World!!!")
