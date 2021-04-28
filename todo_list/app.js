818da33c3a18a0f1944c42369413639c77a959dd
laichangwen <laichangwen21@gmail.com>
2021-04-28 22:22:25 +0800

    feat: add delete todo function

--- todo_list/app.js
+++ todo_list/app.js
@@ -72,6 +72,16 @@ app.post("/todos/:id/edit", (req, res) => {
     .catch(error => console.log(error))
 })
 
+// delete todo of a specific id
+app.post("/todos/:id/delete", (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .then(todo => {
+      todo.remove()
+    })
+    .then(() => res.redirect(`/`))
+    .catch(error => console.log(error))
+})
 
 
 
