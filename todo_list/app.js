cea2c7541e53e14433f942551175c43976f7c5e4
laichangwen <laichangwen21@gmail.com>
2021-05-03 23:26:49 +0800

    feat: add method-override

--- todo_list/app.js
+++ todo_list/app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 const exphbs = require("express-handlebars")
 const bodyParser = require("body-parser")
+const methodOverride = require("method-override")
 const Todo = require("./models/todo")
 const app = express()
 
@@ -9,6 +10,7 @@ app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }))
 app.set("view engine", "hbs")
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 mongoose.connect("mongodb://localhost/todo-list", { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -61,7 +63,7 @@ app.get("/todos/:id/edit", (req, res) => {
     .catch(error => console.log(error))
 })
 
-app.post("/todos/:id/edit", (req, res) => {
+app.put("/todos/:id", (req, res) => {
   const { name, isDone } = req.body
   const id = req.params.id
   return Todo.findById(id)
@@ -75,7 +77,7 @@ app.post("/todos/:id/edit", (req, res) => {
 })
 
 // delete todo of a specific id
-app.post("/todos/:id/delete", (req, res) => {
+app.delete("/todos/:id", (req, res) => {
   const id = req.params.id
   return Todo.findById(id)
     .then(todo => {
